a6e8622d475325c815de08e254c8236d7e43ca05
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-08-25 17:01:48 -0400

    Better

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-	camWidth 		= 320;	// try to grab at this size.
+	/*camWidth 		= 320;	// try to grab at this size.
 	camHeight 		= 240;
 
     //we can now get back a list of devices. 
@@ -20,19 +20,37 @@ void ofApp::setup(){
     
 	vidGrabber.setDeviceID(0);
 	vidGrabber.setDesiredFrameRate(60);
-	vidGrabber.initGrabber(camWidth,camHeight);
+	vidGrabber.initGrabber(camWidth,camHeight);*/
+    
+    serial.setup("/dev/ttyACM0", 9600);
+    
+    server.setup(8888);
+    server.setVerbose(true);
 }
 
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	
-	vidGrabber.update();
+	//vidGrabber.update();
+    
+    if(server.isConnected() && server.getNumClients() > 0) {
+        /*ostringstream convert;
+        convert << server.getLastID();
+        server.sendToAll(convert.str());*/
+        string response = server.receive(0);
+        if(response.length() > 0) {
+            cout << "Receive: " << response << endl;
+            unsigned char* chars = (unsigned char*) response.c_str(); // cast from string to unsigned char*
+            int length = response.length();
+            serial.writeBytes(chars,length);
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	vidGrabber.draw(0,0,720,480);
+	//vidGrabber.draw(0,0,720,480);
 }
 
 
@@ -80,4 +98,39 @@ void ofApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+
+// trim trailing spaces
+string ofxTrimStringRight(string str) {
+    size_t endpos = str.find_last_not_of(" \t\r\n");
+    return (string::npos != endpos) ? str.substr( 0, endpos+1) : str;
+}
+
+// trim trailing spaces
+string ofxTrimStringLeft(string str) {
+    size_t startpos = str.find_first_not_of(" \t\r\n");
+    return (string::npos != startpos) ? str.substr(startpos) : str;
+}
+
+string ofxTrimString(string str) {
+    return ofxTrimStringLeft(ofxTrimStringRight(str));;
+}
+
+
+string ofxGetSerialString(ofSerial &serial, char until) {
+    static string str;
+    stringstream ss;
+    char ch;
+    int ttl=1000;
+    while ((ch=serial.readByte())>0 && ttl-->0 && ch!=until) {
+        ss << ch;
+    }
+    str+=ss.str();
+    if (ch==until) {
+        string tmp=str;
+        str="";
+        return ofxTrimString(tmp);
+    } else {
+        return "";
+    }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxNetwork.h"
 
 class ofApp : public ofBaseApp{
 
@@ -15,11 +16,14 @@ class ofApp : public ofBaseApp{
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+        void gotMessage(ofMessage msg);
 
 		ofVideoGrabber 		vidGrabber;
 		unsigned char * 	videoInverted;
 		ofTexture			videoTexture;
 		int 				camWidth;
 		int 				camHeight;
+    
+        ofxTCPServer        server;
+        ofSerial            serial;
 };
