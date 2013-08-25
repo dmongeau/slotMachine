f2560459b14f6481d2b4f42d665ce3efeb625427
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-08-25 19:18:51 -0400

    Better socket

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,17 +1,53 @@
 #include "ofApp.h"
 
+// trim trailing spaces
+string ofxTrimStringRight(string str) {
+    size_t endpos = str.find_last_not_of(" \t\r\n");
+    return (string::npos != endpos) ? str.substr( 0, endpos+1) : str;
+}
+
+// trim trailing spaces
+string ofxTrimStringLeft(string str) {
+    size_t startpos = str.find_first_not_of(" \t\r\n");
+    return (string::npos != startpos) ? str.substr(startpos) : str;
+}
+
+string ofxTrimString(string str) {
+    return ofxTrimStringLeft(ofxTrimStringRight(str));;
+}
+
+
+string ofxGetSerialString(ofSerial &serial, char until) {
+    static string str;
+    stringstream ss;
+    char ch;
+    int ttl=1000;
+    while ((ch=serial.readByte())>0 && ttl-->0 && ch!=until) {
+        ss << ch;
+    }
+    str+=ss.str();
+    if (ch==until) {
+        string tmp=str;
+        str="";
+        return ofxTrimString(tmp);
+    } else {
+        return "";
+    }
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 	ofBackground(0);
     
-    displayMoney = 0.00;
+    money = 0.00;
+    displayMoney = -0.01;
     
     serial.setup("/dev/ttyACM0", 9600);
     
     server.setup(7678);
     server.setVerbose(true);
     
-    priceText.init("verdana.ttf", 80);
+    priceText.init("courrier.ttf", 120);
     
     priceText.setText("0.00 $");
     
@@ -24,13 +60,13 @@ void ofApp::setup(){
 void ofApp::update(){
     
     if(server.isConnected() && server.getNumClients() > 0) {
-        string response = server.receive(0);
+        string response = ofxTrimString(server.receive(0));
         if(response.length() > 0) {
             cout << "Receive: " << response << endl;
             unsigned char* chars = (unsigned char*) response.c_str(); // cast from string to unsigned char*
             int length = response.length();
-            money += (ofToFloat(response)*0.25);
             serial.writeBytes(chars,length);
+            money += (ofToFloat(response)*0.25);
         }
     }
     
@@ -43,9 +79,9 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    ofSetColor(255);
+    priceText.setColor(255,255,255,255);
     priceText.setText(ofToString(displayMoney,2)+" $");
-    priceText.drawCenter(ofGetWidth()/2,(ofGetHeight()-priceText.getHeight())/2);
+    priceText.drawCenter(ofGetWidth()/2,(ofGetHeight()-priceText.getHeight()*1.5)/2);
     
 }
 
@@ -93,41 +129,6 @@ void ofApp::dragEvent(ofDragInfo dragInfo){
 
 //--------------------------------------------------------------
 void ofApp::exit(){
-    cout << "Close server";
 	server.close();
-}
-
-// trim trailing spaces
-string ofxTrimStringRight(string str) {
-    size_t endpos = str.find_last_not_of(" \t\r\n");
-    return (string::npos != endpos) ? str.substr( 0, endpos+1) : str;
-}
-
-// trim trailing spaces
-string ofxTrimStringLeft(string str) {
-    size_t startpos = str.find_first_not_of(" \t\r\n");
-    return (string::npos != startpos) ? str.substr(startpos) : str;
-}
-
-string ofxTrimString(string str) {
-    return ofxTrimStringLeft(ofxTrimStringRight(str));;
-}
-
-
-string ofxGetSerialString(ofSerial &serial, char until) {
-    static string str;
-    stringstream ss;
-    char ch;
-    int ttl=1000;
-    while ((ch=serial.readByte())>0 && ttl-->0 && ch!=until) {
-        ss << ch;
-    }
-    str+=ss.str();
-    if (ch==until) {
-        string tmp=str;
-        str="";
-        return ofxTrimString(tmp);
-    } else {
-        return "";
-    }
+    cout << "Server closed";
 }
